c84df100c8b26ae48a20658833e898e96b8f4278
ThiagoSchiaffino <thiagoschiaffinodechia@gmail.com>
2023-07-03 15:33:53 -0300

    cambios

--- trabajofinal2.js
+++ trabajofinal2.js
@@ -4,10 +4,10 @@
 
 
 
-
 const inicializarJs = () => {
     const boton = document.getElementById("enviar-btn");
     boton.addEventListener('click', function (e) {
         enviarTareas();
     });
 }
+window.addEventListener('load', inicializarJs);
